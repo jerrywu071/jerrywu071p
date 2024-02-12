ca2cb442625b37d25f61e03d410104f430d15ae2
jerrywu071 <jerrywu071@outlook.com>
2024-02-12 00:52:28 -0500

    change some stuff

--- my-website/docusaurus.config.js
+++ my-website/docusaurus.config.js
@@ -8,8 +8,8 @@ import {themes as prismThemes} from 'prism-react-renderer';
 
 /** @type {import('@docusaurus/types').Config} */
 const config = {
-  title: 'My Site',
-  tagline: 'Dinosaurs are cool',
+  title: "Jerry's portfolio website",
+  tagline: 'Programmer & Musician',
   favicon: 'img/favicon.ico',
 
   // Set the production url of your site here
